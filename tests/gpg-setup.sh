8de6a5b0397b3529de9f630b8207365d4b27b272
Hendri Pretorius <pretorh@gmail.com>
2018-09-30 13:57:48 +0200

    tests: run tests in functions, pretty print their names

--- tests/gpg-setup.sh
+++ tests/gpg-setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 source tests/setup/helpers.sh
 
-# can encrypt/decrypt with the test key
-echo "hello" \
-    | gpg --encrypt -r $GPG_ID $GPG_OPTS \
-    | gpg --decrypt $GPG_OPTS \
-    | grep "hello"
+can_encrypt_and_decrypt_with_the_test_key() {
+    echo "hello" \
+        | gpg --encrypt -r $GPG_ID $GPG_OPTS \
+        | gpg --decrypt $GPG_OPTS \
+        | grep "hello"
+}
+
+run can_encrypt_and_decrypt_with_the_test_key
--- tests/setup/helpers.sh
+++ tests/setup/helpers.sh
@@ -1,7 +1,33 @@
 #!/usr/bin/env bash
 set -e
+set -o pipefail
+
+# gpg
 
 GNUPGHOME=./tests/.gpg
 GPG_OPTS="--passphrase=passphrase --batch --pinentry-mode loopback"
 GPG_ID=foo@bar.com
 export GNUPGHOME GPG_OPTS GPG_ID
+
+# helpers
+
+failed=0
+run() {
+    name="$(echo "$1" | sed -s 's|_| |g')"
+
+    # run function, fail on first error
+    # https://stackoverflow.com/a/33704639/1016377
+    set +e
+    (set -e; $1)
+    error=$?
+    set -e
+
+    ((error)) && echo "[FAIL]: $name" && failed=1 && return
+    echo "[PASS] $name"
+}
+
+finish() {
+    exit $failed
+}
+
+trap finish EXIT
