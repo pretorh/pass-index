27179aec1ada2db49bc040b5cd32a0948ae2beff
Hendri Pretorius <pretorh@gmail.com>
2018-09-30 16:53:06 +0200

    rename the index file name
    to not conflict with `pass index` (an implicit show on the "index" file)
    rename the index file used by pass index to `.index`, which will cause
    `pass index` to either call the extention, or to fail

--- bin/index.bash
+++ bin/index.bash
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 NAME="pass-index v0.0.1"
+INDEX_NAME=.index
 
 echo "$NAME" >&2
 
 if [ "$1" = "show" ] ; then
     read -p "enter name: " -r name
-    id="$(pass show index | grep "$name" | awk -F ' ' '{print $1}')"
+    id="$(pass show $INDEX_NAME | grep "$name" | awk -F ' ' '{print $1}')"
     pass show "$id"
 elif [ -z "$1" ] ; then
-    pass show index | awk -F ' ' '{print $2}'
+    pass show $INDEX_NAME | awk -F ' ' '{print $2}'
 fi
