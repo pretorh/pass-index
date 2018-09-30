9bbc8ad8eb2ea3954e478f9fa3e82ee43a3fafb7
Hendri Pretorius <pretorh@gmail.com>
2018-09-30 20:13:03 +0200

    tests: fix init-test-gpg script
    do not source helpers, just get the gpg homedir from command line

--- tests/setup/init-test-gpg.sh
+++ tests/setup/init-test-gpg.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-source tests/setup/helpers.sh
+set -e
 
 # setup a test gpg home directory and create a secret key
 # based on https://gist.github.com/pretorh/0a4a8192e0b8e8c9a7b574ec1c824224
+GNUPGHOME=$1
+export GNUPGHOME
 
 rm -rf "$GNUPGHOME"
 mkdir -pv "$GNUPGHOME"
