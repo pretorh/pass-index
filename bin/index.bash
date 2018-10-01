44e861a9c6be1afc6662125b69675b6094dc21cd
Hendri Pretorius <pretorh@gmail.com>
2018-10-01 20:51:48 +0200

    extract function, use case for sub command check
    extract functions for sub commands, util functions
    
    use a case statement for the sub command check
    
    add explicit --version to dump the version details

--- bin/index.bash
+++ bin/index.bash
@@ -4,24 +4,46 @@ NAME="pass-index v0.0.1"
 INDEX_NAME=.index
 UUIDGEN=${PASS_INDEX_UUID_GENERATOR-uuidgen}
 
-((PASS_INDEX_SILENT)) || echo "$NAME" >&2
+_cmd_passindex_fail() {
+    ((PASS_INDEX_SILENT)) || echo "$*" >&2
+    exit 1
+}
 
-if [ "$1" = "show" ] ; then
-    read -p "enter name: " -r name
-    id="$(pass show $INDEX_NAME | grep "$name" | awk -F ' ' '{print $1}')"
-    pass show "$id"
-elif [ -z "$1" ] ; then
-    pass show $INDEX_NAME | awk -F ' ' '{print $2}'
-elif [ "$1" = "create" ] ; then
-    read -p "enter name: " -r name
-    id="$($UUIDGEN)"
+_cmd_passindex_update_index() {
     cat << EOF |
 $(pass show $INDEX_NAME 2>/dev/null)
-$id $name
+$1 $2
 EOF
     pass insert --multiline $INDEX_NAME
+}
+
+cmd_passindex_create() {
+    local name id
+    read -r -p "enter name: " name
+    id="$($UUIDGEN)"
+    _cmd_passindex_update_index "$id" "$name"
     pass insert "$id"
-else
-    ((PASS_INDEX_SILENT)) || echo "$NAME: invalid command '$1'" >&2
-    exit 1
+}
+
+cmd_passindex_show() {
+    local name id
+    read -r -p "enter name: " name
+    id="$(pass show $INDEX_NAME | grep "$name" | awk -F ' ' '{print $1}')"
+    pass show "$id"
+}
+
+cmd_passindex_list() {
+    pass show $INDEX_NAME | awk -F ' ' '{print $2}'
+}
+
+if [ -z "$1" ] ; then
+    cmd_passindex_list
+    exit 0
 fi
+
+case "$1" in
+    show)           shift; cmd_passindex_show "$@" ;;
+    create)         shift; cmd_passindex_create "$@" ;;
+    --version)      shift; echo "$NAME" ;;
+    *)              _cmd_passindex_fail "invalid command '$1'" ;
+esac
