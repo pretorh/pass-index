c86fe2bb5f0a7ca6045fa9e0c9bae15d7ea1036f
Hendri Pretorius <pretorh@gmail.com>
2022-06-22 16:44:45 +0200

    tests: skip index filename test when system-wide extension installed

--- tests/10-basic.sh
+++ tests/10-basic.sh
@@ -25,6 +25,10 @@ allow_version() {
 }
 
 run extention_is_loadable
-run index_file_is_named_different_to_not_clash_with_implicit_show_in_pass
+if PASSWORD_STORE_DIR='' PASSWORD_STORE_EXTENSIONS_DIR='' PASSWORD_STORE_ENABLE_EXTENSIONS=false pass index 2>"$LOG_FILE" ; then
+    skip index_file_is_named_different_to_not_clash_with_implicit_show_in_pass "test will incorrectly use system-wide index extension and fail"
+else
+    run index_file_is_named_different_to_not_clash_with_implicit_show_in_pass
+fi
 run fail_on_invalid_sub_command
 run allow_version
